5f3fa40b76c7fec9b0742b9d1e9e1242a6215e47
Nirmal Bhetwal <bhetwalnirmal101@gmail.com>
2022-05-28 12:35:46 -0400

    handled touch events of all buttons

--- FA_nirmal_c0841296_iOS/ViewController.swift
+++ FA_nirmal_c0841296_iOS/ViewController.swift
@@ -23,6 +23,14 @@ class ViewController: UIViewController {
     var player2: Player = Player(name: "Player 2", mark: .CROSS)
     
     @IBOutlet weak var cell00: UIButton!
+    @IBOutlet weak var cell01: UIButton!
+    @IBOutlet weak var cell02: UIButton!
+    @IBOutlet weak var cell10: UIButton!
+    @IBOutlet weak var cell11: UIButton!
+    @IBOutlet weak var cell12: UIButton!
+    @IBOutlet weak var cell20: UIButton!
+    @IBOutlet weak var cell21: UIButton!
+    @IBOutlet weak var cell22: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,9 +41,54 @@ class ViewController: UIViewController {
         
     }
     
+    // handle the touch event of the buttons
     @IBAction func onClickCell00(_ sender: UIButton) {
-        let row = 0
-        let column = 0
+        var row = 0
+        var column = 0
+        
+        // check which button was pressed
+        switch sender {
+            case cell00:
+                row = 0
+                column = 0
+                break
+            case cell01:
+                row = 0
+                column = 1
+                break
+            case cell02:
+                row = 0
+                column = 2
+                break
+            case cell10:
+                row = 1
+                column = 0
+                break
+            case cell11:
+                row = 1
+                column = 1
+                break
+            case cell12:
+                row = 1
+                column = 2
+                break
+            case cell20:
+                row = 2
+                column = 0
+                break
+            case cell21:
+                row = 2
+                column = 1
+                break
+            case cell22:
+                row = 2
+                column = 2
+                break
+            default:
+                print("default")
+                break
+        }
+        
         let mark = getCurrentPlayer().mark!
         
         board[row][column] = mark
@@ -46,7 +99,7 @@ class ViewController: UIViewController {
             imageName = "cross"
         }
         
-        cell00.setImage(UIImage(named: imageName), for: .normal)
+        sender.setImage(UIImage(named: imageName), for: .normal)
     }
     
     @IBAction func onClickCell01(_ sender: UIButton) {
