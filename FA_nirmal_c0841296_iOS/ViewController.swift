f9703effe195920185da05acb121bda7b29e8cc4
Nirmal Bhetwal <bhetwalnirmal101@gmail.com>
2022-05-27 20:39:55 -0400

    updated board

--- FA_nirmal_c0841296_iOS/ViewController.swift
+++ FA_nirmal_c0841296_iOS/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let ROW = 3
+let COLUMN = 3
 
+let NOUGHT: Int = 1
+let CROSS: Int = 2
+let EMPTY: Int? = nil
+
+class ViewController: UIViewController {
+    // initialize the board variable
+    var board: [[Int?]] = [[Int?]](repeating: [Int?](repeating: EMPTY, count: ROW), count: COLUMN)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // initialize the board
+        initializeBoard()
     }
-
-
+    
+    func initializeBoard () {
+        for i in 0 ..< ROW {
+            for j in 0 ..< COLUMN {
+                print("\(i), \(j): \(board[i][j])")
+            }
+        }
+    }
 }
 
